de32781ca9d031481eb927729f7374a45f7355b5
praneshmeher <praneshmeher@gmail.com>
2021-11-27 12:15:17 +0530

    commit

---
+++ src/multiplyFloat/Example.java
@@ -0,0 +1,13 @@
+package multiplyFloat;
+
+public class Example {
+
+	public static void main(String args[]) {
+		float f1= 10;
+		float f2 = 20.5f;
+		
+		float ans = f1*f2;
+		System.out.println(ans);
+	}
+	
+}
---
+++ src/readAndPrint/Example.java
@@ -0,0 +1,15 @@
+package readAndPrint;
+
+import java.util.Scanner;
+
+public class Example {
+	
+	public static void main(String args[]) {
+		
+		int num;
+		System.out.println("Enter the number");
+		Scanner s = new Scanner(System.in);
+		num = s.nextInt();
+		System.out.println(num);
+	}
+}
